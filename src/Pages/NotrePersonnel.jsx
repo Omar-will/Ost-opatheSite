2158c8985137b209bc6c06684c1c0e3a8e5e5ce4
Omar-will <omar.bengoudifa@outlook.fr>
2024-09-24 10:50:57 +0200

    picture

--- src/Pages/NotrePersonnel.jsx
+++ src/Pages/NotrePersonnel.jsx
@@ -18,26 +18,46 @@ function NotrePersonnel() {
               <h3>{doctor.name}</h3>
               <h4>{doctor.specialty}</h4>
               <p>{doctor.bio}</p>
-              <h5>Formations</h5>
-              <ul>
-                {doctor.formations.map((formation, idx) => (
-                  <li key={idx}>{formation}</li>
-                ))}
-              </ul>
-              <h5>Expériences</h5>
-              <ul>
-                {doctor.experiences.map((experience, idx) => (
-                  <li key={idx}>{experience}</li>
-                ))}
-              </ul>
-              <h5>Associations</h5>
-              <p>{doctor.associations}</p>
-              <h5>Travaux et Publications</h5>
-              <ul>
-                {doctor.publications.map((publication, idx) => (
-                  <li key={idx}>{publication}</li>
-                ))}
-              </ul>
+
+              {doctor.formations && doctor.formations.length > 0 && (
+                <>
+                  <h5>Formations</h5>
+                  <ul>
+                    {doctor.formations.map((formation, idx) => (
+                      <li key={idx}>{formation}</li>
+                    ))}
+                  </ul>
+                </>
+              )}
+
+              {doctor.experiences && doctor.experiences.length > 0 && (
+                <>
+                  <h5>Expériences</h5>
+                  <ul>
+                    {doctor.experiences.map((experience, idx) => (
+                      <li key={idx}>{experience}</li>
+                    ))}
+                  </ul>
+                </>
+              )}
+
+              {doctor.associations && (
+                <>
+                  <h5>Associations</h5>
+                  <p>{doctor.associations}</p>
+                </>
+              )}
+
+              {doctor.publications && doctor.publications.length > 0 && (
+                <>
+                  <h5>Travaux et Publications</h5>
+                  <ul>
+                    {doctor.publications.map((publication, idx) => (
+                      <li key={idx}>{publication}</li>
+                    ))}
+                  </ul>
+                </>
+              )}
             </div>
           </div>
         ))}
